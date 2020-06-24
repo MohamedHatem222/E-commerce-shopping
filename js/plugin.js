8624fb1f984c717cddf2e13e3a52f5a4df703abb
Hettaa <62189990+Hettaa@users.noreply.github.com>
2020-06-24 17:21:21 +0200

    copy

---
+++ js/plugin.js
@@ -0,0 +1,99 @@
+$(document).ready(function() {
+    $(".header-right .menu").mouseover(function() {
+        $(".header-right .header-languages").css("display", "block");
+    });
+    $(".header-right .header-languages").mouseleave(function() {
+        setTimeout(function() {
+            $(".header-right .header-languages").css("display", "none");
+        }, 200);
+
+    });
+
+    $(".navigation-icons .cart-icon").mouseover(function() {
+        $(".sub-menu").css("display", "block");
+    });
+    $(".sub-menu").mouseleave(function() {
+        $(this).css("display", "none");
+    });
+
+
+    //slick
+    $('.slick-slider').slick({
+        dots: true,
+        infinite: true,
+        speed: 500,
+        fade: true,
+        cssEase: 'linear'
+    });
+
+    $(".slick-dots li button").on("click", function(e) {
+        e.preventDefault();
+        $(".slick-slide .bg5 .bg-inner5 .left-side").animate({
+            top: '20%'
+        }, 1000, function() {
+            setTimeout(function() {
+
+                $(".slick-slide .bg5 .bg-inner5 .left-side").animate({
+                    top: '120%'
+                }, 2000)
+            }, 4000);
+        });
+        $(".slick-slide .bg5 .bg-inner5 .right-side").animate({
+            top: "9%"
+        }, 2000, function() {
+            setTimeout(function() {
+
+                $(".slick-slide .bg5 .bg-inner5 .right-side").animate({
+                    top: '120%'
+                }, 2000)
+            }, 5000);
+        });
+        $(".slick-slide .bg5 .bg-inner5 .right-side .second-mob").animate({
+            top: "40%"
+        }, 3000, function() {
+            setTimeout(function() {
+
+                $(".slick-slide .bg5 .bg-inner5 .right-side .second-mob").animate({
+                    top: '120%'
+                }, 2000)
+            }, 6000);
+        });
+    });
+
+    $(".slick-dots li button").on("click", function(e) {
+        e.preventDefault();
+        $(".slick-slide .bg4 .bg-inner4 .center-img").animate({
+            top: '20%'
+        }, 2000, function() {
+            setTimeout(function() {
+                $(".slick-slide .bg4 .bg-inner4 .center-img").animate({
+                    top: '120%'
+                }, 2000);
+            }, 6000);
+        });
+    });
+
+    //owl-carousel
+    $('.owl-carousel').owlCarousel({
+        loop: true,
+        margin: 20,
+        nav: true,
+        responsive: {
+            0: {
+                items: 1
+            },
+            600: {
+                items: 3
+            },
+            1000: {
+                items: 4
+            }
+        }
+    })
+
+
+
+
+
+
+});
